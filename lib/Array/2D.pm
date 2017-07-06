3e0b2d95ffb626bcddd40ae64bd15fa68f18a50d
Aaron Priven <aaron@priven.com>
2017-07-05 23:31:16 -0700

    Checking in changes prior to tagging of version 0.001_004.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index fcb19ac..1a94bc6 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Array-2D
    
     {{$NEXT}}
    
    +0.001_004 2017-07-06T06:31:05Z
    +
    +     - Added new code to use "\n" instead of "\R" as the line
    +       delimiter when running under perls before 5.10.
    +
     0.001_003 2017-07-06T06:13:12Z
    
         - Corrected the loading of test modules so it works

--- lib/Array/2D.pm
+++ lib/Array/2D.pm
@@ -5,7 +5,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = '0.001_003';
+our $VERSION = '0.001_004';
 $VERSION = eval $VERSION;   ## no critic (BuiltinFunctions::ProhibitStringyEval)
 
 ## no critic (RequirePodAtEnd)
@@ -18,7 +18,7 @@ Array::2D - Methods for simple array-of-arrays data structures
 
 =head1 VERSION
 
-This documentation refers to version 0.001_003
+This documentation refers to version 0.001_004
 
 =head2 NOTICE
 
